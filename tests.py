cc0d0635db78a2bf267d14d6f466e7319dbf72d4
unknown <tw15220@my.bristol.ac.uk>
2018-04-18 21:47:15 +0100

    meh

--- tests.py
+++ tests.py
@@ -11,7 +11,6 @@ import journey
 import numpy as np
 np.set_printoptions(threshold=np.inf)
 
-<<<<<<< HEAD
 sizeA = (5,5)
 startA = [0,0]
 endA = [2,2]
@@ -25,7 +24,7 @@ print(test1.get_state())
 ##print(test1.get_heuristic())
 #result1 = search.find_path(test1,startA,endA)
 ##print(result1)
-=======
+
 sizeA = (3,3)
 startA = [0,0]
 endA = [2,2]
@@ -39,12 +38,12 @@ test1.update_heuristic(endA)
 #print(test1.get_heuristic())
 result1 = search.find_path(test1,startA,endA)
 print(result1)
->>>>>>> 7e14af7dbf8df0cea4b01a19a2b670978b4d2091
+
 ##test2 = grid_object.grid(sizeA,startA,endA,[[1,1]])
 ##result2 = search.find_path(test2,startA,endA)
 
 
-<<<<<<< HEAD
+
 #sizeB = (10,10)
 #ob1 = [[3,4],[4,4],[3,3],[4,5]]
 #ob2 = [[6,8],[7,8],[8,8],[9,8]]
@@ -63,33 +62,33 @@ print(result1)
 #env.update_heuristic(endB)
 #
 #results3 = journey.simulation(inf,env,[0,0],[9,9])
-=======
-sizeB = (10,10)
-ob1 = [[3,4],[4,4],[3,3],[4,5]]
-ob2 = [[6,8],[7,8],[8,8],[9,8]]
-obstacles = ob1+ob2
-startB = [0,0]
-endB = [9,9]
-#
-inf = grid_object.grid(sizeB,[],startB,endB)
-inf.update_risk()
-inf.update_heuristic(endB)
 
-env = grid_object.grid(sizeB,obstacles,startB,endB)
-#print(env.get_state())
+#sizeB = (10,10)
+#ob1 = [[3,4],[4,4],[3,3],[4,5]]
+#ob2 = [[6,8],[7,8],[8,8],[9,8]]
+#obstacles = ob1+ob2
+#startB = [0,0]
+#endB = [9,9]
+##
+#inf = grid_object.grid(sizeB,[],startB,endB)
+#inf.update_risk()
+#inf.update_heuristic(endB)
+#
+#env = grid_object.grid(sizeB,obstacles,startB,endB)
+##print(env.get_state())
+##env.show_me()
+#env.update_risk()
+#env.update_heuristic(endB)
+##
+#
+#move_prob1 = [1,0,0,0,0,0,0,0]
+#move_prob2 = [0.9,0.1,0,0,0,0,0,0]
+#
+#results4 = journey.simulation(inf,env,[0,0],[9,9],move_prob2)
+#env.update_path_colour(results4[2])
 #env.show_me()
-env.update_risk()
-env.update_heuristic(endB)
-#
+#print('BWANANA!!!!!x2')
 
-move_prob1 = [1,0,0,0,0,0,0,0]
-move_prob2 = [0.9,0.1,0,0,0,0,0,0]
-
-results4 = journey.simulation(inf,env,[0,0],[9,9],move_prob2)
-env.update_path_colour(results4[2])
-env.show_me()
-print('BWANANA!!!!!x2')
->>>>>>> 7e14af7dbf8df0cea4b01a19a2b670978b4d2091
 #env.update_path_colour (results3[1])
 #env.show_me()
 ##print(results3)

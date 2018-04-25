e7f83587569c016307288e5b719adcaeaa25bfaa
JDL-Vortex <jl13563@my.bristol.ac.uk>
2018-04-25 14:37:38 +0100

    modification to active work files

--- roadmap.py
+++ roadmap.py
@@ -19,17 +19,19 @@ street1 = [[0,0],[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[8,1],[8,2],[8,
 [0,3],[1,3],[2,3],[3,3],[4,3],[5,3],[5,4],[5,5],[5,6],[5,7],[5,8],[5,9],[5,10]]
 obstacles = os.hroad(3,0) + os.vroad(0,3)
 
-inf = grid_object.grid(sizeB,[],startB,endB)
-inf.update_risk()
-inf.update_heuristic(endB)
 
-env = grid_object.grid(sizeB,obstacles,startB,endB)
+env = grid_object.grid(sizeB,obstacles,len(obstacles))
 env.update_risk()
 env.update_heuristic(endB)
 
+inf = grid_object.grid(sizeB,[],len(obstacles))
+inf.update_risk()
+inf.update_heuristic(endB)
+
 move_prob1 = [1,0,0,0,0,0,0,0]
 move_prob2 = [0.9,0.1,0,0,0,0,0,0]
+move_prob3 = [0.8,0.1,0,0,0,0,0,0.1]
 
-results3 = journey.simulation(inf,env,startB,endB,move_prob1)
-env.update_path_colour(results3[2])
+results3 = journey.simulation(inf,env,startB,[endB,endC],move_prob1)
+env.update_path_colour(results3[2],startB,[endB,endC])
 env.show_me()
